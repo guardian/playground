1e335b4ac52de4f05105aaf9ddea2464c8f67123
Max Duval <max.duval@theguardian.com>
2022-05-11 10:24:14 +0100

    feat(remap): helper to remap prefixes

--- scripts/labels.ts
+++ scripts/labels.ts
@@ -18,29 +18,38 @@ const { data: labels } = await octokit.rest.issues.listLabelsForRepo({
 const prefixes = {
   tech: labs[400],
   type: sport[400],
-  started: culture[500],
+  context: culture[500],
   community: lifestyle[500],
   opinion: opinion[500],
   news: news[500],
   fixme: neutral[0],
 } as const;
 
+/** In case we need to change a prefix in the future,
+ * this helper will take care of renaming all prefixes */
+const remaps: Record<string, keyof typeof prefixes | undefined> = {
+  started: "context",
+};
+
 const isPrefix = (prefix: string): prefix is keyof typeof prefixes =>
   prefix in prefixes;
 
 for (const label of labels) {
-  const [prefix] = label.name.split(": ");
+  const [prefix, ...rest] = label.name.split(": ");
 
   if (!isPrefix(prefix)) {
     console.warn("invalid prefix: ", prefix);
+
+    const new_name = `${remaps[prefix]}: ${rest.join(" ")}` ??
+      `fixme: ${label.name}`;
 
     const {
       data: { name, color },
     } = await octokit.rest.issues.updateLabel({
       ...defaultParams,
       name: label.name,
-      new_name: `fixme: ${label.name}`,
-      color: prefixes["fixme"].slice(1),
+      new_name,
+      color: remaps[prefix] ? undefined : prefixes["fixme"].slice(1),
     });
 
     console.info("new name & colour: ", [name, color]);
