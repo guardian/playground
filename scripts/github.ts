a38e714cff575ae2b9c9adb49bb03bb009411e6c
Max Duval <max.duval@theguardian.com>
2022-05-16 10:59:38 +0100

    refactor(github): use plugin directly

--- scripts/github.ts
+++ scripts/github.ts
@@ -1,7 +1,6 @@
 import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
-import { Octokit } from "https://cdn.skypack.dev/octokit";
-import type { RestEndpointMethodTypes } from "https://cdn.skypack.dev/@octokit/plugin-rest-endpoint-methods?dts";
-import type { Endpoints } from "https://cdn.skypack.dev/@octokit/types?dts";
+import { Octokit as OctokitCore } from "https://cdn.skypack.dev/@octokit/core?dts";
+import { restEndpointMethods } from "https://cdn.skypack.dev/@octokit/plugin-rest-endpoint-methods?dts";
 
 console.log(config({ safe: true, export: true }));
 
@@ -9,28 +8,10 @@ console.log(config({ safe: true, export: true }));
 const token = Deno.env.get("GITHUB_TOKEN");
 if (!token) throw new Error("Missing GITHUB_TOKEN");
 
-type OctokitWithRest = {
-  rest: {
-    [Section in keyof RestEndpointMethodTypes]: {
-      [Method in keyof RestEndpointMethodTypes[Section]]:
-        RestEndpointMethodTypes[Section][Method] extends
-          { parameters: infer Parameters; response: infer Response } ? (
-          arg: Parameters,
-        ) => Promise<Response>
-          : never;
-    };
-  };
-  request: <Url extends keyof Endpoints>(
-    url: Url,
-    arg: Endpoints[Url]["parameters"],
-  ) => Promise<Endpoints[Url]["parameters"]>;
-};
+const Octokit = OctokitCore.plugin(restEndpointMethods);
 
-/**
- * A hydrated Octokit with types for the rest API.
- */
-// @ts-expect-error -- Octokit’s own types are not as good as ours
-const octokit = new Octokit({ auth: token }) as OctokitWithRest;
+/** A hydrated Octokit */
+const octokit = new Octokit({ auth: token });
 
 const defaultParams = {
   owner: "guardian",
